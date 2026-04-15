b53cb45c18499f77a75a21a8b6e992d02f0c6b13
Théophile "Inkurey22" R. <inkurey22.tr@proton.me>
2026-04-15 11:29:55 +0200

    1.1.0

--- flake.nix
+++ flake.nix
@@ -1,20 +1,31 @@
 {
-  description = "SoulBoard development environment";
+  description = "SoulBoard development environment with cross-compilation support";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
-        devShells.default = pkgs.mkShell {
+        fenixPkgs = fenix.packages.${system};
+        
+        # Rust toolchain with cross-compilation targets
+        rustToolchain = fenixPkgs.stable.toolchain;
+        windowsRustToolchain = fenixPkgs.combine [
+          fenixPkgs.stable.toolchain
+          fenixPkgs.targets.x86_64-pc-windows-gnu.stable.rust-std
+        ];
+        
+        # Linux x86_64 native development
+        linuxDevShell = pkgs.mkShell {
+          name = "soulboard-linux-dev";
           buildInputs = with pkgs; [
-            cargo
-            rustc
+            rustToolchain
             pkg-config
             libxkbcommon
             wayland
@@ -44,6 +55,29 @@
               pkgs.vulkan-loader
             ]}:$LD_LIBRARY_PATH
           '';
+        };
+        
+        # Windows x86_64 cross-compilation (from Linux)
+        windowsDevShell = pkgs.mkShell {
+          name = "soulboard-windows-dev";
+          buildInputs = with pkgs; [
+            windowsRustToolchain
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+            pkg-config
+          ];
+
+          shellHook = ''
+            export CARGO_BUILD_TARGET=x86_64-pc-windows-gnu
+            export CC_x86_64_pc_windows_gnu=x86_64-w64-mingw32-gcc
+            export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER=x86_64-w64-mingw32-gcc
+          '';
+        };
+      in {
+        devShells = {
+          default = linuxDevShell;
+          linux = linuxDevShell;
+          windows = windowsDevShell;
         };
       });
 }
